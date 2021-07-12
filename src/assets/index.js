e79ffe5f01391e3476da4af54c19e3fb81739b1e
ganesh <ganeshgajula22@gmail.com>
2021-07-12 15:52:26 +0530

    add: icons

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+export {
+  HomeIcon,
+  NotificationIcon,
+  BookmarkIcon,
+  ProfileIcon,
+  ReplyIcon,
+  RepostIcon,
+  LikeIcon,
+  AddToBookmarkIcon,
+  SearchIcon,
+  VerifiedBadgeIcon,
+  MoreIcon,
+} from "./svgs";
