833808df51bce821befe5e5dfa6c54ea0cfbf410
ganesh <ganeshgajula22@gmail.com>
2021-07-12 16:02:45 +0530

    chore: react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h2>Social Media using Redux</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
