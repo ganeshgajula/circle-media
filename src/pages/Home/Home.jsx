5b1fa250f9b55e5a7ecac7b3a8f06f6a107653a8
ganesh <ganeshgajula22@gmail.com>
2021-07-13 17:34:33 +0530

    refactor: component positions

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { SideNavbar } from "../../components";
+import { SideNavbar, NewPost } from "../../components";
+import { TitleIcon } from "../../assets";
 
 export const Home = () => {
   return (
-    <div>
+    <div className="flex w-full m-auto">
       <SideNavbar />
+      <main className="w-5/12 border-r border-gray-100">
+        <div className="flex justify-between items-center px-3 py-3 border-b border-gray-100">
+          <span className="text-xl font-extrabold">Home</span>
+          <TitleIcon />
+        </div>
+        <NewPost />
+      </main>
     </div>
   );
 };
