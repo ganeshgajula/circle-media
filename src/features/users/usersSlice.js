97ff46a6aca8d8f86ff2b728d19b620df7692db0
ganesh <ganeshgajula22@gmail.com>
2021-07-18 15:14:19 +0530

    add: dummy user data

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -3,16 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   users: [
     {
+      userId: 1,
       name: "Tanay Pratap",
       userName: "tanaypratap",
     },
     {
+      userId: 2,
       name: "Swapnil Agarwal",
       userName: "swap",
     },
     {
+      userId: 3,
       name: "Akanksha Choudhary",
       userName: "akanksha_ch",
+    },
+    {
+      userId: 4,
+      name: "Dan Abramov",
+      userName: "dan_abramov",
+    },
+    {
+      userId: 5,
+      name: "Kent C. Dodds",
+      userName: "kentcdoods",
     },
   ],
 };
