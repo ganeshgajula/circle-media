0e18620e525903e93041a772d6c6e60a71cf5681
ganesh <ganeshgajula22@gmail.com>
2021-07-18 09:12:07 +0530

    style: user details

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -4,21 +4,35 @@ import { useSelector } from "react-redux";
 export const SearchResults = ({ searchedKeyword }) => {
   const users = useSelector((state) => state.users);
 
-  const matchedUsersList = users.users.filter((user) =>
-    user.userName.toLowerCase().includes(searchedKeyword.toLowerCase())
+  const matchedUsersList = users.users.filter(
+    (user) =>
+      user.userName.toLowerCase().includes(searchedKeyword.toLowerCase()) ||
+      user.name.toLowerCase().includes(searchedKeyword.toLowerCase())
   );
 
   return (
-    <div className="w-80 h-96 bg-white shadow-md rounded-lg p-4">
+    <div className="inline-block w-80 h-auto max-h-96 bg-white shadow-md rounded-lg px-3 pt-2 overflow-y-auto">
       {searchedKeyword && matchedUsersList.length ? (
-        matchedUsersList.map((user) => (
-          <div>
-            <h3>{user.name}</h3>
-            <p>{user.userName}</p>
-          </div>
-        ))
+        matchedUsersList.map((user) => {
+          let initials = "";
+          user.name.split(" ").map((username) => (initials += username[0]));
+
+          return (
+            <div className="flex items-center space-x-4 mb-3">
+              <div className="h-12 w-12 bg-blue-500 text-white rounded-full flex items-center justify-center">
+                <span className="text-xl font-semibold">{initials}</span>
+              </div>
+              <div className="flex flex-col">
+                <h3 className="font-semibold">{user.name}</h3>
+                <p className="text-gray-500">@{user.userName}</p>
+              </div>
+            </div>
+          );
+        })
       ) : (
-        <p>{!matchedUsersList.length ? "No user found" : "Search Users"}</p>
+        <p className="font-medium text-gray-500 mb-2">
+          {!matchedUsersList.length ? "No user found" : "Search Users"}
+        </p>
       )}
     </div>
   );
