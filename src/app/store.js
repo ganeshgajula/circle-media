e4a76b40e7b9d7d5c08dcb2026a76fa3afe02836
ganesh <ganeshgajula22@gmail.com>
2021-08-09 17:13:14 +0530

    add: authSlice reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postSlice";
 import usersReducer from "../features/users/usersSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     feed: postsReducer,
     users: usersReducer,
+    auth: authReducer,
   },
 });
