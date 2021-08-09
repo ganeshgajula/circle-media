c1a71ddf07da46c45a7889ae2eed99fb25cb44e8
ganesh <ganeshgajula22@gmail.com>
2021-08-09 17:08:52 +0530

    feat: auth Slice with login, signup

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,96 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const signUpUser = createAsyncThunk(
+  "auth/signUpUser",
+  async (userCredentials, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/users/signup",
+        userCredentials
+      );
+      return response.data.savedUser;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (userCredentials, thunkAPI) => {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "http://localhost:4000/users/authenticate",
+        headers: userCredentials,
+      });
+      return response.data.user;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const initialState = {
+  isUserLoggedIn: false,
+  status: "idle",
+  currentUser: JSON.parse(localStorage?.getItem("userCredentials")) || null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [signUpUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [signUpUser.fulfilled]: (state) => {
+      state.status = "fulfilled";
+      toast.success("Signup successful! Kindly login with given credentials", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 3000,
+      });
+    },
+    [signUpUser.rejected]: (state, action) => {
+      state.status = "rejected";
+      toast.error(action.payload.message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 5000,
+      });
+    },
+    [loginUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      state.status = "fulfilled";
+      state.isUserLoggedIn = true;
+      state.currentUser = action.payload;
+      toast.success("Login successful", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 2200,
+      });
+      localStorage?.setItem(
+        "userCredentials",
+        JSON.stringify({
+          userId: action.payload._id,
+          username: action.payload.username,
+          firstname: action.payload.firstname,
+          lastname: action.payload.lastname,
+          email: action.payload.email,
+        })
+      );
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.status = "rejected";
+      toast.error(action.payload.message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 2200,
+      });
+    },
+  },
+});
+
+export default authSlice.reducer;
