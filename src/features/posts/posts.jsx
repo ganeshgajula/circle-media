b25337eb3d8e62bca7d07d0eeac815b6872908c5
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-16 23:44:59 +0530

    feat: generate user feed

--- src/features/posts/posts.jsx
+++ src/features/posts/posts.jsx
@@ -2,25 +2,46 @@ import React, { useEffect } from "react";
 import { NewPost } from "./NewPost";
 import { useDispatch, useSelector } from "react-redux";
 import { PostCard } from "./PostCard";
-import { loadPosts } from "./postSlice";
+import { loadAllPosts, loadPosts } from "./postSlice";
 import { EmptyPosts } from "./EmptyPosts";
 
 export const Posts = () => {
-  const { status, posts } = useSelector((state) => state.feed);
-  const {
-    currentUser: { _id: userId },
-  } = useSelector((state) => state.auth);
-  const sortedPosts = posts
+  const dispatch = useDispatch();
+  const { status, posts, feed } = useSelector((state) => state.feed);
+  const { currentUser } = useSelector((state) => state.auth);
+  // const sortedPosts = posts
+  //   ?.slice()
+  //   .sort((a, b) => b.postDate.localeCompare(a.postDate));
+
+  const followingUsersDocs = feed?.filter((user) =>
+    currentUser?.following.includes(user.userId)
+  );
+
+  let addFollowingUserPosts = [];
+  const followingUserPosts = followingUsersDocs.map((user) =>
+    addFollowingUserPosts.concat(user.posts)
+  );
+
+  const finalFeed = [];
+  followingUserPosts.map((userPosts) =>
+    userPosts.map((post) => finalFeed.push(post))
+  );
+
+  const sortedPosts = finalFeed
     ?.slice()
     .sort((a, b) => b.postDate.localeCompare(a.postDate));
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (status === "idle") {
-      dispatch(loadPosts(userId));
+      dispatch(loadPosts(currentUser._id));
+      dispatch(loadAllPosts());
     }
-  }, [dispatch, status, userId]);
+  }, [dispatch, status, currentUser._id]);
 
+  console.log(feed);
+  console.log(followingUsersDocs);
+  console.log(followingUserPosts);
+  console.log(finalFeed);
   console.log(posts);
   console.log(sortedPosts);
 
@@ -29,10 +50,10 @@ export const Posts = () => {
       <NewPost />
       <div className="h-3 bg-extra-light-gray"></div>
       <div>
-        {sortedPosts?.length > 0 ? (
-          sortedPosts?.map((post) => <PostCard post={post} key={post._id} />)
-        ) : (
+        {sortedPosts.length === 0 && status === "fulfilled" ? (
           <EmptyPosts />
+        ) : (
+          sortedPosts?.map((post) => <PostCard post={post} key={post._id} />)
         )}
       </div>
     </>
