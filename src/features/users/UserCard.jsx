f5c0b62ff26a70f321c5285afc94b44038ad00e0
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-15 15:45:29 +0530

    fix: naviagte on follow

--- src/features/users/UserCard.jsx
+++ src/features/users/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { isUserPresent } from "../../utils/utils";
 import { followUnfollowUser } from "./usersSlice";
 
@@ -9,15 +9,16 @@ export const UserCard = ({ firstname, lastname, username, _id }) => {
     currentUser: { following, _id: currentLoggedInUserId },
   } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const firstNameInitial = firstname[0];
   const lastNameInitial = lastname[0];
   const userInitials = `${firstNameInitial}${lastNameInitial}`;
 
   return (
-    <Link
-      to={`/profile/${username}`}
-      className="flex items-center justify-between p-3 hover:bg-gray-50"
+    <div
+      onClick={() => navigate(`/profile/${username}`)}
+      className="flex items-center justify-between p-3 hover:bg-gray-50 cursor-pointer"
     >
       <span className="flex items-center">
         <span className="bg-blue-500 mr-4 text-white h-12 w-12 flex items-center justify-center rounded-full ">
@@ -37,18 +38,19 @@ export const UserCard = ({ firstname, lastname, username, _id }) => {
               ? "border border-blue-400 text-primary hover:bg-blue-50"
               : "bg-primary text-white py-1"
           } font-semibold rounded-2xl px-4 py-1 `}
-          onClick={() =>
+          onClick={(e) => {
+            e.stopPropagation();
             dispatch(
               followUnfollowUser({
                 username: username,
                 currentLoggedInUserId,
               })
-            )
-          }
+            );
+          }}
         >
           {!isUserPresent(following, _id) ? "Follow" : "Following"}
         </button>
       )}
-    </Link>
+    </div>
   );
 };
